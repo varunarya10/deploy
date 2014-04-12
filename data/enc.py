259904a19e2f92b1aeaeb4d40ad2cff4cfa0ce70
Soren Hansen <Soren.Hansen@ril.com>
2014-04-12 18:24:00 +0000

    Use hardcoded path for encdata

--- data/enc.py
+++ data/enc.py
@@ -2,5 +2,5 @@
 import sys
 import yaml
 
-data = yaml.load(open(sys.argv[1], 'r'))
-print yaml.dump(data[sys.argv[2]], default_flow_style=False)
+data = yaml.load(open('/etc/puppet/encdata.yaml', 'r'))
+print yaml.dump(data[sys.argv[1].split('.')[0]], default_flow_style=False)
