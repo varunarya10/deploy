5d9ff4eea05b2e8083e6daf6b3e3823a043b9efa
Soren Hansen <Soren.Hansen@ril.com>
2014-04-13 16:36:19 +0000

    Make encdata filename an argument

--- bin/finish_node.sh
+++ bin/finish_node.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 if [ $# -ne 2 ]
 then
-    echo "Usage: $0 environment.yaml puppetdir"
+    echo "Usage: $0 environment.yaml encdata.yaml puppetdir"
     exit 1
 fi
 
-for node in $(python -c 'import yaml; d = yaml.load(open("'$1'", "r")); print " ".join([x["ip"] for x in d.values()])')
+environment="$1"
+encdata="$2"
+puppetdir="$3"
+
+for node in $(python -c 'import yaml; d = yaml.load(open("'"${environment}"'", "r")); print " ".join([x["ip"] for x in d.values()])')
 do
     cd $2
 	tar cz . | ssh ubuntu@$node 'rm -rf modules || true; mkdir modules ; cd modules ; tar xz'
     cd -
-    scp -r data/enc.py encdata.yaml ubuntu@$node:
+    scp -r data/enc.py ${encdata} ubuntu@$node:
     ssh ubuntu@${node} 'sudo rm -rf /etc/puppet/modules ; \
                         sudo mv modules /etc/puppet/modules ; \
                         sudo mv enc.py /etc/puppet/enc.py ; \
